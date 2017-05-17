f4f7e83fcae0923e548bb937ff6a2d325cee944a
itsjbao <maplefn84@gmail.com>
2017-05-17 11:17:30 -0700

    Delete Start.java

--- Start.java
+++
@@ -1,9 +0,0 @@
-
-public class Start {
-	public static void main(String[] args){
-		GameScreen ref=new GameScreen();
-		World screen = new World(ref);
-		ref.setWorld(screen);
-		screen.initJFrame();
-	}
-}
