0d0f241499c031a5df2ccab5c4a483a744cf2fe9
itsjbao <maplefn84@gmail.com>
2017-05-17 11:18:16 -0700

    Add files via upload

--- Final Project/src/GameScreen.java
+++ Final Project/src/GameScreen.java
@@ -1,22 +1,21 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 
 public class GameScreen {
-	private JLabel label1;
-	public static void main(String[] args) {
-		GameScreen screen = new GameScreen();
-		screen.initJFrame();
+	World ref;
+	
+	public GameScreen(){
+		ref=null;
 	}
 	
-	public void initJFrame(){
-		JFrame jframe = new JFrame();
-		
-		label1 = new JLabel("Hello World", SwingConstants.CENTER);
-		
-		jframe.add(label1, BorderLayout.CENTER);
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.setSize(800, 800);
-		jframe.setVisible(true);
+	public void setWorld(World temp){
+		ref=temp;
 	}
 }
---
+++ Final Project/src/Obstacle.java
@@ -0,0 +1,33 @@
+import java.awt.Rectangle;
+
+public class Obstacle {
+	private double x, y, height, width;
+
+	public Obstacle(double x, double y, double height, double width) {
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.width = width;
+	}
+	
+	public Rectangle returnPlayer() {
+		Rectangle rect = new Rectangle((int) x,(int) y,(int) height, (int) width);
+		return rect;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+}
