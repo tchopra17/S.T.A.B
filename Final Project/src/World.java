bc18c5e489f7c953b4f2c5411f2ef465354ee844
kevin5000w <kevin5001w@gmail.com>
2017-05-18 10:51:39 -0700

    Update World.java

--- Final Project/src/World.java
+++ Final Project/src/World.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -5,86 +6,125 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JFrame;
-import javax.swing.Timer;
+import javax.swing.*;
 
-
-public class World extends JFrame implements KeyListener, ActionListener{
+public class World extends JFrame implements KeyListener, ActionListener {
 	Timer t = new Timer(5, this);
 	private final int SCREEN_WIDTH = 600, SCREEN_HEIGHT = 600;
-	double x = 0, y = 0 ,velx = 0, vely = 0;
-	Player p1 = new Player(x, y, 50, 50);
-	private GameScreen ref;
-	
-	public World(GameScreen temp){
-		ref=temp;
+	static double x = 0;
+	static double y = 0;
+	double velx = 0;
+	double vely = 0;
+	static int randno = (int) (5 * Math.random() + 1);
+	boolean didGameLoad;
+
+	Player p1 = new Player(x, y, 30, 30);
+	Obstacle o1 = new Obstacle(x, y, (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+	Obstacle o2 = new Obstacle(x, y, (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+	Obstacle o3 = new Obstacle(x, y, (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+	Obstacle o4 = new Obstacle(x, y, (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+
+	public GameScreen() {
 		t.start();
+		didGameLoad = true;
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(true);
 	}
-	public void initJFrame(){
+
+	public void initJFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		setVisible(true);
 	}
-	
-	public void paint(Graphics g){
+
+	public void paint(Graphics g) {
 		p1.setX(x);
 		p1.setY(y);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.fill(p1.returnPlayer());
+
+		if (didGameLoad == true) {
+			o1.setX((int) (500 * Math.random() + 50));
+			o1.setY((int) (500 * Math.random() + 50));
+			Graphics2D g3 = (Graphics2D) g;
+			g3.fill(o1.returnPlayer());
+
+			o2.setX((int) (500 * Math.random() + 50));
+			o2.setY((int) (500 * Math.random() + 50));
+			Graphics2D g4 = (Graphics2D) g;
+			g4.fill(o2.returnPlayer());
+
+			o3.setX((int) (500 * Math.random() + 50));
+			o3.setY((int) (500 * Math.random() + 50));
+			Graphics2D g5 = (Graphics2D) g;
+			g5.fill(o3.returnPlayer());
+
+			o4.setX((int) (500 * Math.random() + 50));
+			o4.setY((int) (500 * Math.random() + 50));
+			Graphics2D g6 = (Graphics2D) g;
+			g6.fill(o4.returnPlayer());
+			didGameLoad = false;
+		}
+
 	}
 
-	public void setScreen(GameScreen temp){
-		ref=temp;
+	public static void main(String[] args) {
+		GameScreen screen = new GameScreen();
+		screen.initJFrame();
+		Player p1 = new Player(x, y, 50, 50);
+
 	}
-	
-	public void upPressed(){
+
+	public void upPressed() {
 		vely = -0.5;
 		velx = 0;
 	}
-	public void downPressed(){
+
+	public void downPressed() {
 		vely = 0.5;
 		velx = 0;
 	}
-	public void rightPressed(){
+
+	public void rightPressed() {
 		vely = 0;
 		velx = 0.5;
 	}
-	public void leftPressed(){
+
+	public void leftPressed() {
 		vely = 0;
 		velx = -0.5;
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		int key = e.getKeyCode();
-		
-		if (key == KeyEvent.VK_UP){
+
+		if (key == KeyEvent.VK_UP) {
 			upPressed();
 		}
-		if (key == KeyEvent.VK_DOWN){
+		if (key == KeyEvent.VK_DOWN) {
 			downPressed();
 		}
-		if (key == KeyEvent.VK_LEFT){
+		if (key == KeyEvent.VK_LEFT) {
 			leftPressed();
 		}
-		if (key == KeyEvent.VK_RIGHT){
-			rightPressed(); 
+		if (key == KeyEvent.VK_RIGHT) {
+			rightPressed();
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
