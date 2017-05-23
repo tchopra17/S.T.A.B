35b8eadc87caad082c6f73c21e907e47d3959866
Tushar Chopra <tusharchopra17@gmail.com>
2017-05-22 19:35:16 -0700

    Removed not working project folder and fixed player movement

--- NewFinal/src/World.java
+++ NewFinal/src/World.java
@@ -9,12 +9,10 @@ import java.awt.event.KeyListener;
 import javax.swing.*;
 public class World extends JPanel implements KeyListener, ActionListener {
 	Timer t = new Timer(5, this);
-	private final int SCREEN_WIDTH = 600, SCREEN_HEIGHT = 600;
 	static double x = 0;
 	static double y = 0;
 	double velx = 0;
 	double vely = 0;
-	static int randno = (int) (5 * Math.random() + 1);
 
 	Player p1 = new Player(x, y, 30, 30, 0);
 	Obstacle o1 = new Obstacle((int) (500 * Math.random() + 50), (int) (500 * Math.random() + 50), (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
@@ -32,16 +30,15 @@ public class World extends JPanel implements KeyListener, ActionListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		p1.setX(x);
-		p1.setY(y);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.fill(p1.returnPlayer());
+		
 		g2.fill(o1.returnPlayer());
 		g2.fill(o2.returnPlayer());
 		g2.fill(o3.returnPlayer());
 		g2.fill(o4.returnPlayer());
 	}
-
+	
 	public void upPressed(){
 		p1.accelerate();
 	}
