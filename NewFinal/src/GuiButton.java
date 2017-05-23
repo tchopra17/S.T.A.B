4d7ab0f5f9ea3d62812817bd351e6deb65637d09
BeckyWMiller <becky.rm2000@gmail.com>
2017-05-23 00:34:20 -0700

    Add files via upload

---
+++ NewFinal/src/GuiButton.java
@@ -0,0 +1,120 @@
+package screens;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.lang.Thread.State;
+import java.util.ArrayList;
+
+public class GuiButton {
+
+	private State currentState = State.RELEASED;
+	private Rectangle clickBox;
+	private ArrayList<ActionListener> actionListeners;
+	private String text = "";
+			
+	private Color r;
+	private Color h;
+	private Color p;
+	private Font font = new Font("Bebas Neue Regular", Font.BOLD, 22);
+	//private AudioHandler audio;
+	
+	public GuiButton(int x, int y, int width, int height){
+		clickBox = new Rectangle (x, y, width, height);
+		actionListeners = new ArrayList<ActionListener>();
+		r = new Color(173,177, 179);
+		h = new Color(150, 156, 158);
+		p = new Color(111, 116,117);
+		
+		//audio = AudioHandler.gerInstance();
+		//audio.load("select.wave", "select");
+	}
+	
+	public void update(){}
+	
+	public void render (Graphics2D g){
+		if(currentState == State.RELEASED){
+			g.setColor(r);
+			g.fill(clickBox);
+		}
+		else if(currentState == State.HOVER){
+			g.setColor(h);
+			g.fill(clickBox);
+		}
+		else if(currentState == State.PRESSED){
+			g.setColor(p);
+			g.fill(clickBox);
+		}
+		g.setColor(Color.white);
+		g.setFont(font);
+		g.drawString(text, clickBox.x + clickBox.width / 2, clickBox.y + clickBox.height / 2);
+	}
+	
+	public void addActionListener(ActionListener listener){
+		actionListeners.add(listener);
+	}
+	
+	public void mousePressed(MouseEvent e){
+		if (clickBox.contains(e.getPoint())){
+			currentState = State.PRESSED;
+		}
+	}
+
+public void mouseReleased(MouseEvent e){
+	if (clickBox.contains(e.getPoint())){
+		for(ActionListener al : actionListeners){
+			al.actionPerformed(null);
+		}
+	}
+		currentState = State.RELEASED;
+}
+	
+public void mouseDragged(MouseEvent e){
+	if (clickBox.contains(e.getPoint())){
+		currentState = State.PRESSED;
+	}
+	else{
+		currentState = State.RELEASED;
+	}
+}
+
+public void mouseMoved(MouseEvent e){
+	if (clickBox.contains(e.getPoint())){
+		currentState = State.HOVER;
+	}
+	else{
+		currentState = State.RELEASED;
+	}
+
+}
+
+public int getX(){
+	return clickBox.x;
+}
+
+public int getY(){
+	return clickBox.y;
+}
+
+public int getWidth(){
+	return clickBox.width;
+}
+
+public int getHeight(){
+	return clickBox.height;
+}
+
+public void setText(String text){
+	this.text = text;
+}
+private enum State{
+		RELEASED,
+		HOVER,
+		PRESSED
+	}
+	
+	
+}
---
+++ NewFinal/src/GuiScreen.java
@@ -0,0 +1,74 @@
+package screens;
+
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+
+public class GuiScreen {
+
+	private static GuiScreen screen;
+	private HashMap<String, GuiPanel> panels;
+	private String currentPanel = "";
+	
+	
+private GuiScreen(){
+	panels = new HashMap<String, GuiPanel>();
+}
+
+public static GuiScreen getinstance(){
+	if (screen == null){
+		screen = new GuiScreen();
+	}
+	return screen;
+}
+
+public void update(){
+	if(panels.get(currentPanel) != null){
+		panels.get(currentPanel).update();
+	}
+	}
+	
+	public void render(Graphics2D g){
+		if(panels.get(currentPanel) != null){
+			panels.get(currentPanel).render(g);
+		}
+
+}
+	
+	public void add(String panelName, GuiPanel panel){
+		panels.put(panelName, panel);
+		
+	}
+	
+	public void setCurrentPanel(String PanelName){
+		currentPanel = PanelName;
+	}
+	
+	public void mousePressed(MouseEvent e){
+		if(panels.get(currentPanel) != null){
+			panels.get(currentPanel).mousePressed(e);
+		}	
+	}
+
+public void mouseReleased(MouseEvent e){
+	if(panels.get(currentPanel) != null){
+		panels.get(currentPanel).mouseReleased(e);
+	}	
+
+	}
+	
+public void mouseDragged(MouseEvent e){
+	if(panels.get(currentPanel) != null){
+		panels.get(currentPanel).mouseDragged(e);
+	}	
+
+}
+
+public void mouseMoved(MouseEvent e){
+	if(panels.get(currentPanel) != null){
+		panels.get(currentPanel).mouseMoved(e);
+	}	
+
+}
+
+}
