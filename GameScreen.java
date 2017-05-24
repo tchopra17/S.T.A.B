299ff986c384589f5f4053eb0fe89c3f4f9da361
BeckyWMiller <becky.rm2000@gmail.com>
2017-05-24 10:28:37 -0700

    Add files via upload

---
+++ GameScreen.java
@@ -0,0 +1,123 @@
+package project;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.*;
+
+public class GameScreen extends JPanel implements MouseListener, MouseMotionListener{
+	private World ref;
+	private boolean running;
+	private Thread game;
+	private GuiScreen screen;
+	public static final int SCREEN_WIDTH = 600, SCREEN_HEIGHT = 600;
+	private BufferedImage image = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+			
+	public GameScreen(){
+		ref = null;
+		setFocusable(true);
+		setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		
+		screen = GuiScreen.getinstance();
+		screen.add("Menu",new MainMenu());
+		screen.setCurrentPanel("Menu");
+	}
+	
+	public void update(){
+		screen.update();
+	}
+	
+	/*public static void main(String[] args){
+		MainMenu menu = new MainMenu();
+		menu.render(null);
+		menu.update();
+	}
+	*/
+	public void render(){
+		Graphics2D g = (Graphics2D) image.getGraphics();
+		g.setColor(Color.white);
+		g.fillRect(0, 0, SCREEN_WIDTH,  SCREEN_HEIGHT);
+		screen.render(g);
+		g.dispose();
+		
+		Graphics2D g2d = (Graphics2D)getGraphics();
+		g2d.drawImage(image, 0, 0, null);
+		g2d.dispose();
+	}
+	/*public void initJFrame() {
+		setDefaultCloseOperation(JPanel.EXIT_ON_CLOSE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setVisible(true);
+	}
+	*/
+	public void setWorld(World temp){
+		ref=temp;
+		add(ref);
+	}
+	
+	public void start(){
+		if(running){
+			return;
+		}
+		running = true;
+		game = new Thread("game");
+		game.start();
+	}
+	
+	public void stop(){
+		if(!running){
+			return;
+		}
+		running = false;
+		System.exit(0);
+	}
+	
+	
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		screen.mouseDragged(e);
+		
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		screen.mouseMoved(e);
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		screen.mousePressed(e);
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		screen.mouseReleased(e);
+		
+	}
+}
---
+++ World.java
@@ -0,0 +1,95 @@
+package project;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+public class World extends JPanel implements KeyListener, ActionListener {
+	Timer t = new Timer(5, this);
+	static double x = 0;
+	static double y = 0;
+	double velx = 0;
+	double vely = 0;
+
+	Player p1 = new Player(x, y, 30, 30, 0);
+	Obstacle o1 = new Obstacle((int) (500 * Math.random() + 50), (int) (500 * Math.random() + 50), (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+	Obstacle o2 = new Obstacle((int) (500 * Math.random() + 50), (int) (500 * Math.random() + 50), (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+	Obstacle o3 = new Obstacle((int) (500 * Math.random() + 50), (int) (500 * Math.random() + 50), (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+	Obstacle o4 = new Obstacle((int) (500 * Math.random() + 50), (int) (500 * Math.random() + 50), (int)(60 * Math.random() + 20), (int)(60 * Math.random() + 20));
+
+	public World(GameScreen temp) {
+		t.start();
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(true);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.fill(p1.returnPlayer());
+		
+		g2.fill(o1.returnPlayer());
+		g2.fill(o2.returnPlayer());
+		g2.fill(o3.returnPlayer());
+		g2.fill(o4.returnPlayer());
+	}
+	
+	public void upPressed(){
+		p1.accelerate();
+	}
+	public void downPressed(){
+		p1.deaccelerate();
+	}
+	public void rightPressed(){
+		p1.turnRight();
+	}
+	public void leftPressed(){
+		p1.turnLeft();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_UP) {
+			upPressed();
+		}
+		if (key == KeyEvent.VK_DOWN) {
+			downPressed();
+		}
+		if (key == KeyEvent.VK_LEFT) {
+			leftPressed();
+		}
+		if (key == KeyEvent.VK_RIGHT) {
+			rightPressed();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		repaint();
+		p1.setX(p1.getX() + p1.getVelocity()*Math.acos(p1.getDirection()*(Math.PI/180.)));
+		p1.setY(p1.getY() + p1.getVelocity()*Math.asin(p1.getDirection()*(Math.PI/180.)));
+	}
+}
